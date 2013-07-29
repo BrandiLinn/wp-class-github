164b08bde1252e6a030260a392b373b9a340dc13
Brandi Johnecheck <brandi.johne@gmail.com>
2013-07-29 15:05:33 -0700

    note need to do

--- hw2/addthis/index.php
+++ hw2/addthis/index.php
@@ -5,13 +5,14 @@ Description: Modification of the Smart Layers plugin from AddThis.com.
 
 <!-- AddThis Smart Layers BEGIN -->
 <!-- Go to http://www.addthis.com/get/smart-layers to customize -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a"></script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a">//switch for enqueue as soos as I can find an explanation that makes any sense to me</script> 
 <script type="text/javascript">
   addthis.layers({
     'theme' : 'transparent',
     'share' : {
       'position' : 'left',
       'numPreferredServices' : 5
+      //admin panel needs to be able to change 'position' and 'numPreferredServices'
     }   
   });
 </script>
