ccc6e79705b30e96e6f52ee3df5ef8e20a4bec66
Brandi Johnecheck <brandi.johne@gmail.com>
2013-07-29 15:14:47 -0700

    guesswork

--- hw2/addthis/index.php
+++ hw2/addthis/index.php
@@ -1,11 +1,23 @@
+<?php
+
 /*
 Plugin Name: AddThis with options
 Description: Modification of the Smart Layers plugin from AddThis.com.
 */
 
+function my_scripts_method() {
+	wp_enqueue_script(
+		'addthis-script',
+		'//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a'
+	);
+}
+
+?>
+
 <!-- AddThis Smart Layers BEGIN -->
 <!-- Go to http://www.addthis.com/get/smart-layers to customize -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a">//switch for enqueue as soos as I can find an explanation that makes any sense to me</script> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a">//switch for enqueue as soos as I can find an explanation that makes any sense to me
+</script> -->
 <script type="text/javascript">
   addthis.layers({
     'theme' : 'transparent',
@@ -17,3 +29,5 @@ Description: Modification of the Smart Layers plugin from AddThis.com.
   });
 </script>
 <!-- AddThis Smart Layers END -->
+
+<?php
