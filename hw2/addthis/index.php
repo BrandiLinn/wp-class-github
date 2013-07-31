79038c62474d15e48699526a7067756b7f354345
Brandi Johnecheck <brandi.johne@gmail.com>
2013-07-30 21:47:24 -0700

    GARLFLAGL!

--- hw2/addthis/index.php
+++ hw2/addthis/index.php
@@ -13,8 +13,15 @@ function addthis_script() {
 }
 
 add_action( 'wp_enqueue_scripts', 'addthis_script' ); //adds the script
-?>
 
+
+//Settings converted into PHP? Stored? 
+//Don't know how to tell the thing where to look for its settings.
+//Can't get share tool to even appear, but enqueue is working. 
+//Tool seems to appear in the loaded HTML, but does not display.
+
+
+/*
 <!-- AddThis Smart Layers BEGIN -->
 <!-- Go to http://www.addthis.com/get/smart-layers to customize
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6d46a734d459a">//switch for enqueue as soon as I can find an explanation that makes any sense to me
@@ -30,16 +37,4 @@ add_action( 'wp_enqueue_scripts', 'addthis_script' ); //adds the script
   });
 </script>-->
 <!-- AddThis Smart Layers END -->
-
-<?php
-
-function addthis_adder( $content ) {
-    if ( is_single() ) {
-        $addthis_settings  = "<script type='text/javascript'> addthis.layers({'theme' : 'transparent', 'share' : {'position' : 'left', 'numPreferredServices' : 5'} });</script>";
-
-        $content .= $addthis_settings;
-    }
-    return $content;
-}
-
-add_filter( 'the_content', 'addthis_adder', 1 ); //appends the thing
+*/
